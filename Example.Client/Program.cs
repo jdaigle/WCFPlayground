a6548a0e545628c4a9b81b2df645e9fdf839d230
jdaigle <joseph@cridion.com>
2010-07-28 20:48:54 -0400

    initial

---
+++ Example.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Example.Service;
+
+namespace Example.Client {
+    class Program {
+        static void Main(string[] args) {
+            var service = ChannelFactory<IService1>.CreateChannel(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:12345/Service1.svc"));
+            var value = service.GetData(5);
+        }
+    }
+}
